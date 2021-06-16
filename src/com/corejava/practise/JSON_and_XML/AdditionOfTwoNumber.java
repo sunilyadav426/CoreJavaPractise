47aee47358912eb815d995b08ae654b56257181c
sunilyadav426 <sunilyadav426@gmail.com>
2021-06-16 13:52:19 +0530

    Addition of 2 no program

---
+++ src/com/corejava/practise/JSON_and_XML/AdditionOfTwoNumber.java
@@ -0,0 +1,19 @@
+package com.corejava.practise.JSON_and_XML;
+
+public class AdditionOfTwoNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  System.out.println("Enter two numbers");
+		    int first = 10;
+		    int second = 20;
+		    
+		    System.out.println(first + " " + second);
+
+		    // add two numbers
+		    int sum = first + second;
+		    System.out.println("The sum is: " + sum);
+		  }
+	}
+
+
